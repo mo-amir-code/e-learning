f630577940d8b86fdfd67925eda410f7c8c7cc4b
gazabb-code <mo.amir.code@gmail.com>
2023-09-17 04:42:18 +0530

    update 1

--- src/Pages/CoursePage.js
+++ src/Pages/CoursePage.js
@@ -3,6 +3,7 @@ import Courses from '../components/Courses'
 import Teachers from '../components/Teachers'
 import StudentReview from '../components/StudentReview'
 import Footer from '../components/Footer'
+import { Link } from 'react-router-dom'
 
 const CoursePage = () => {
   return (
@@ -11,6 +12,11 @@ const CoursePage = () => {
       <Courses/>
       <Teachers/>
       <StudentReview/>
+      <div className='flex justify-center items-center' >
+      <Link to={'/'} className='py-3 my-4 px-10 rounded-2xl bg-orange-500 text-white font-medium shadow-md' >
+        Go Back Home
+      </Link>
+      </div>
       </div>
       <Footer/>
     </div>
